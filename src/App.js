fbb75e6c04e18453dd4172cc46757604e0e335a2
ezekiel <ezekielnizamani0@gmail.com>
2022-10-22 11:59:45 -0700

    added cart icon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import "./App.css";
 import Header from "./components/header/Header";
+import CartIcon from './assets/images/icons/cart-icon.svg'
 function App() {
   return (
     <div className="App">
-    <Header/>
-    <nav className="navigation">
-    <div className="wrapper flex justify-between">
-    <div className="search_box">
-    <input type="search" placeholder="find something you love" className="serach-input"/>
-    </div>
-    <ul className="flex items-center navigation-items pl-5">
-    <li className="navigation-item"><a href="#">Bedroom</a></li>
-    <li className="navigation-item pl-7"><a href="#">living room</a></li>
-    <li className="navigation-item pl-7"><a href="#">kitchen & dinning</a></li>
-    <li className="navigation-item pl-7"><a href="#">Sign in / sign up</a></li>
-   
-    </ul>
-    </div>
-    </nav>
+      <Header />
+      <nav className="navigation">
+        <div className="wrapper flex justify-between">
+          <div className="search_box">
+            <input
+              type="search"
+              placeholder="find something you love"
+              className="serach-input"
+            />
+          </div>
+          <ul className="flex items-center navigation-items ml-5">
+            <li className="navigation-item">
+              <a href="#">Bedroom</a>
+            </li>
+            <li className="navigation-item ml-7">
+              <a href="#">living room</a>
+            </li>
+            <li className="navigation-item ml-7">
+              <a href="#">kitchen & dinning</a>
+            </li>
+            <li className="navigation-item ml-7">
+              <a href="#">Sign in / sign up</a>
+            </li>
+            <li className="navigation-item ml-7">
+          <img src={CartIcon} />
+            </li>
+          </ul>
+        </div>
+      </nav>
     </div>
   );
 }
